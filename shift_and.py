e61a470fa929ee05b4425a67751e926b283af009
rxs <rxs@pkudlib.org>
2013-04-30 02:39:09 +0800

    add shift_and & shift_or

---
+++ shift_and.py
@@ -0,0 +1,33 @@
+#coding=utf8
+
+def preprocess(t):
+    B = {}
+    for i in xrange(len(t)):
+        c = t[i]
+        if c in B:
+            B[c] |= (1 << i)
+        else:
+            B[c] = 1 << i
+    return B
+
+def shift_and_match(s, t):
+    B = preprocess(t)
+    D = 0
+    m = len(t)
+    mask = 1 << (m - 1)
+    res = []
+    for i in xrange(len(s)):
+        c = s[i]
+        if c in B:
+            D = ((D << 1) | 1) & B[c]
+        else:
+            D = 0
+        if (D & mask):
+            res.append(i - m + 1)
+    return res
+
+if __name__ == '__main__':
+    s = raw_input("Source string:\n")
+    t = raw_input("Pattern string:\n")
+    res = shift_and_match(s, t)
+    print 'Find "%s" in "%s" by positions: ' % (t, s), res
